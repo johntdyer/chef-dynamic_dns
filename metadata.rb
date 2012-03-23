e53bd7ea25cc46e0178a69e7b2abe1308cba5aea
Erik Hollensbe <erik+github@hollensbe.org>
2012-03-23 01:14:03 -0700

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "erik+chef@hollensbe.org"
 license           "Apache 2.0"
 description       "Provides methods to handle dynamic DNS updates"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.0"
+version           "0.1.1"
 
 recipe "dynamic_dns::install_nsupdate", 
   "Installs dnsutils + a script to pull the IP from jsonip.com and send updates to a DNS server."
